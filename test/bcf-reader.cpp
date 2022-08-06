7dcc90c2a8d1178ef11a2ffeaef71d3c1fe2efe3
Zilong-Li <zimusen94@gmail.com>
2022-08-06 15:10:26 +0100

    use vcfpp.h instead

--- test/bcf-reader.cpp
+++ test/bcf-reader.cpp
@@ -1,9 +1,9 @@
-#include "SeqLib/BcfReader.h"
+#include "SeqLib/vcfpp.h"
 #include "catch.hh"
 #include <iostream>
 
-using namespace SeqLib;
 using namespace std;
+using namespace vcfpp;
 
 TEST_CASE("Parsing VCF with specific tag", "[bcf-reader]")
 {
@@ -16,54 +16,56 @@ TEST_CASE("Parsing VCF with specific tag", "[bcf-reader]")
     vector<char> gatk;
     vector<bool> gt;
     int n = 0;
-    while (br.GetNextVariant(v))
+    while (br.getNextVariant(v))
     {
         n++;
-        v.GetGenotypes(gt);
+        v.getGenotypes(gt);
         for (auto i : gt) {
             cout << i << ":";
         }
         cout << endl;
 
-        v.GetFormat(ad, "AD");
+        v.getFormat("AD", ad);
         for (auto i : ad) {
             cout << i << ":";
         }
         cout << endl;
 
-        v.GetFormat(gq, "GQ");
+        v.getFormat("GQ", gq);
         for (auto i : gq) {
             cout << i << ":";
         }
         cout << endl;
 
-        v.GetFormat(gatk, "GATK");
-        for (int i = 0; i < v.header->nsamples; i++) {
-            cout << string(gatk.begin()+ i*v.shape1, gatk.begin() + i * v.shape1 + v.shape1 - 1) << ":";
+        v.getFormat("GATK", gatk);
+        for (int i = 0; i < br.nsamples; i++) {
+            cout << string(gatk.begin()+ i*v.nvalues, gatk.begin() + i * v.nvalues + v.nvalues - 1) << ":";
         }
         cout << endl;
 
     }
-    std::shared_ptr<BcfHeader> h = std::make_shared<BcfHeader>(br.header);
-    h->RemoveContig("1");
-    h->RemoveContig("16");
-    h->RemoveFilter("LowQual");
-    h->RemoveInfo("TRAILING");
-    cout << h->AsString() << endl;
 
     BcfReader br2("../htslib/test/index.vcf");
     BcfRecord v2(br2.header);
-    br2.GetNextVariant(v2);
-    v2.RemoveInfo<int>("DP");
-    br2.header.AddInfo("Str", "1", "String", "this is a test for adding string in INFO");
-    v2.SetInfo("Str", string{"S1S2"});
-    v2.SetInfo("Str", string{"str"});
-    v2.SetInfo("DP", vector<int>{1,2});
-    v2.SetInfo("DP", 2);
+    br2.getNextVariant(v2);
+    v2.removeINFO("DP");
+    br2.header.addINFO("Str", "1", "String", "this is a test for adding string in INFO");
+    v2.setINFO("Str", string{"S1S2"});
+    v2.setINFO("Str", string{"str"});
+    vector<int> dp;
+    v2.setINFO("DP", vector<int>{1,2});
+    v2.getINFO("DP", dp);
+    for (auto i : dp) {
+        cout << i << endl;
+    }
+    v2.setINFO("DP", 2);
+    v2.getINFO("DP", dp);
+    for (auto i : dp) {
+        cout << i << endl;
+    }
 
-    cout << br2.header.AsString() << endl;
-    cout << v2.GetInfo<float>("DP") << endl;
-    cout << v2.AsString() << endl;
+    cout << br2.header << endl;
+    cout << v2.asString() << endl;
 
     REQUIRE(n == 10);
 }
@@ -74,11 +76,11 @@ TEST_CASE("Parsing VCF", "[bcf-reader]")
     BcfRecord v(br.header);
     vector<bool> gt;
     int n = 0;
-    while (br.GetNextVariant(v))
+    while (br.getNextVariant(v))
     {
-        v.GetGenotypes(gt);
+        v.getGenotypes(gt);
         REQUIRE(gt.size() == 2504 * 2);
-        if(v.isAllPhased) n++;
+        if(v.allPhased()) n++;
     }
     REQUIRE(n == 3786);
 }
@@ -89,11 +91,11 @@ TEST_CASE("Parsing VCF with subset samples", "[bcf-reader]")
     BcfRecord v(br.header);
     vector<bool> gt;
     int n = 0;
-    while (br.GetNextVariant(v))
+    while (br.getNextVariant(v))
     {
-        v.GetGenotypes(gt);
+        v.getGenotypes(gt);
         REQUIRE(gt.size() == 20);
-        if(v.isAllPhased) n++;
+        if(v.allPhased()) n++;
     }
     REQUIRE(n == 3786);
 }
@@ -104,86 +106,12 @@ TEST_CASE("Parsing VCF in target region", "[bcf-reader]")
     BcfRecord v(br.header);
     vector<bool> gt;
     int n = 0;
-    while (br.GetNextVariant(v))
+    while (br.getNextVariant(v))
     {
-        v.GetGenotypes(gt);
+        v.getGenotypes(gt);
         REQUIRE(gt.size() == 2504 * 2);
-        if(v.isAllPhased) n++;
+        if(v.allPhased()) n++;
     }
     REQUIRE(n == 3243);
 }
 
-TEST_CASE("Parsing VCF with subset samples in target region", "[bcf-reader]")
-{
-    BcfReader br("chr20.2000001.2100000.vcf.gz", "HG00107,HG00108,HG00109,HG00110,HG00111,HG00112,HG00113,HG00114,HG00115,HG00116", "chr20:2006060-");
-    BcfRecord v(br.header);
-    vector<bool> gt;
-    int n = 0;
-    while (br.GetNextVariant(v))
-    {
-        v.GetGenotypes(gt);
-        REQUIRE(gt.size() == 20);
-        if(v.isAllPhased) n++;
-    }
-    REQUIRE(n == 3243);
-}
-
-TEST_CASE("Parsing BCF", "[bcf-reader]")
-{
-    BcfReader br("chr20.2000001.2100000.bcf.gz");
-    BcfRecord v(br.header);
-    vector<int> gt;
-    int n = 0;
-    while (br.GetNextVariant(v))
-    {
-        v.GetGenotypes(gt);
-        REQUIRE(gt.size() == 2504 * 2);
-        if(v.isAllPhased) n++;
-    }
-    REQUIRE(n == 3786);
-}
-
-TEST_CASE("Parsing BCF in target region", "[bcf-reader]")
-{
-    BcfReader br("chr20.2000001.2100000.vcf.gz", "-", "chr20");
-    BcfRecord v(br.header);
-    vector<int> gt;
-    int n = 0;
-    while (br.GetNextVariant(v))
-    {
-        v.GetGenotypes(gt);
-        REQUIRE(gt.size() == 2504 * 2);
-        if(v.isAllPhased) n++;
-    }
-    REQUIRE(n == 3786);
-}
-
-TEST_CASE("Parsing BCF with subset samples", "[bcf-reader]")
-{
-    BcfReader br("chr20.2000001.2100000.bcf.gz", "HG00107,HG00108,HG00109,HG00110,HG00111,HG00112,HG00113,HG00114,HG00115,HG00116");
-    BcfRecord v(br.header);
-    vector<bool> gt;
-    int n = 0;
-    while (br.GetNextVariant(v))
-    {
-        v.GetGenotypes(gt);
-        REQUIRE(gt.size() == 20);
-        if(v.isAllPhased) n++;
-    }
-    REQUIRE(n == 3786);
-}
-
-TEST_CASE("Parsing BCF with subset samples in target region", "[bcf-reader]")
-{
-    BcfReader br("chr20.2000001.2100000.bcf.gz", "HG00107,HG00108,HG00109,HG00110,HG00111,HG00112,HG00113,HG00114,HG00115,HG00116", "chr20:2006060-2010000");
-    BcfRecord v(br.header);
-    vector<bool> gt;
-    int n = 0;
-    while (br.GetNextVariant(v))
-    {
-        v.GetGenotypes(gt);
-        REQUIRE(gt.size() == 20);
-        if(v.isAllPhased) n++;
-    }
-    REQUIRE(n == 129);
-}
