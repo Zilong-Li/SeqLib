b0b81344733f643d56ed1fc31cdd841b55fabfd2
Zilong-Li <zimusen94@gmail.com>
2022-08-06 11:18:25 +0100

    use vcfpp.h instead

--- test/bcf-writer.cpp
+++ test/bcf-writer.cpp
@@ -1,74 +1,64 @@
-#include "SeqLib/BcfReader.h"
-#include "SeqLib/BcfWriter.h"
 #include "catch.hh"
 #include <iostream>
+#include "SeqLib/vcfpp.h"
 
-using namespace SeqLib;
 using namespace std;
+using namespace vcfpp;
 
 TEST_CASE("Test bcf-writer", "[bcf-writer]")
 {
     BcfReader br("../htslib/test/test-vcf-hdr-in.vcf");
-    std::cout << br.header.AsString() << std::endl;
+    std::cout << br.header << std::endl;
 
-    br.header.AddSample("NA11111");
+    br.header.addSample("NA11111");
     try {
-        auto vec = br.header.GetSeqnames();
+        auto vec = br.header.getSeqnames();
     } catch (std::runtime_error & e) {
         std::cout << e.what() << "start adding contig 1 in header\n\n";
-        br.header.AddContig("1");
+        br.header.addContig("1");
     }
-    std::cout << br.header.AsString() << std::endl;
-    auto vec = br.header.GetSeqnames();
+    std::cout << br.header.asString() << std::endl;
+    auto vec = br.header.getSeqnames();
     for (auto& i : vec)
     {
         std::cout << i << std::endl;
     }
 
     BcfWriter bw("test.vcf");
-    bw.WriteHeader();
+    bw.writeHeader();
 
 }
 
 TEST_CASE("Write BCF with custome header and variants", "[bcf-writer]")
 {
     BcfWriter bw("test.bcf");
-    bw.InitalHeader();
-    bw.header.AddFormat("GT", "1", "String", "Genotype");
-    bw.header.AddInfo("AF", "A", "Float", "Estimated allele frequency in the range (0,1)");
-    bw.header.AddContig("chr20");
-    bw.header.AddSample("NA12878");
-    bw.WriteLine("chr20\t2006060\trs146931526\tG\tC\t100\tPASS\tAF=0.000998403\tGT\t1|0");
+    bw.initalHeader();
+    bw.header.addFORMAT("GT", "1", "String", "Genotype");
+    bw.header.addINFO("AF", "A", "Float", "Estimated allele frequency in the range (0,1)");
+    bw.header.addContig("chr20");
+    bw.header.addSample("NA12878");
+    bw.writeLine("chr20\t2006060\trs146931526\tG\tC\t100\tPASS\tAF=0.000998403\tGT\t1|0");
 }
 
 TEST_CASE("Write VCF with custome header and variants", "[bcf-writer]")
 {
     BcfWriter bw("test.vcf");
-    bw.InitalHeader("VCF4.3");
-    bw.header.AddFormat("GT", "1", "String", "Genotype");
-    bw.header.AddInfo("AF", "A", "Float", "Estimated allele frequency in the range (0,1)");
-    bw.header.AddContig("chr20");
-    bw.header.AddSample("NA12878");
-    bw.WriteLine("chr20\t2006060\trs146931526\tG\tC\t100\tPASS\tAF=0.000998403\tGT\t1|0");
+    bw.initalHeader("VCF4.3");
+    bw.header.addFORMAT("GT", "1", "String", "Genotype");
+    bw.header.addINFO("AF", "A", "Float", "Estimated allele frequency in the range (0,1)");
+    bw.header.addContig("chr20");
+    bw.header.addSample("NA12878");
+    bw.writeLine("chr20\t2006060\trs146931526\tG\tC\t100\tPASS\tAF=0.000998403\tGT\t1|0");
 }
 
-TEST_CASE("Write VCF with custome header", "[bcf-writer]")
-{
-    BcfWriter bw("test.vcf");
-    bw.InitalHeader();
-    bw.header.AddContig("21");
-    bw.header.AddFormat("GT", "1", "String", "Genotype");
-    bw.header.AddInfo("AF", "A", "Float", "Estimated allele frequency in the range (0,1)");
-    bw.WriteHeader();
-}
 
 TEST_CASE("Write VCF by copying header from another VCF", "[bcf-writer]")
 {
     BcfReader br("../htslib/test/test-vcf-hdr-in.vcf");
     BcfRecord v(br.header);
     BcfWriter bw("test.vcf");
-    bw.InitalHeader(br.header);
-    bw.WriteHeader();
-    br.GetNextVariant(v);
-    bw.WriteRecord(v);
+    bw.initalHeader(br.header);
+    bw.writeHeader();
+    br.getNextVariant(v);
+    bw.writeRecord(v);
 }
